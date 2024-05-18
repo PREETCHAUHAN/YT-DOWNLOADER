629572d32af9733b1ce8818c69705bfa4d00f2e6
DJ Preet <33035404+PREETCHAUHAN@users.noreply.github.com>
2024-05-18 13:12:58 +0530

    Update script.py

--- Youtube/script.py
+++ Youtube/script.py
@@ -7,9 +7,9 @@ class Translation(object):
 ├📛 **My Name** : [YouTube Video Uploader Bot](https://t.me/YouTubeUploaderOneBot)
 ├📢 **Framework** : [Pyrogram 2.0.106](https://docs.pyrogram.org/)
 ├💮 **Language** : [Python 3.12.3](https://www.python.org)
-├👥 **Support Group** : [NT BOTS SUPPORT](https://t.me/NT_BOTS_SUPPORT)
-├🥏 **Channel** : [NT BOT CHANNEL](https://t.me/NT_BOT_CHANNEL)
-├⛲ **Source** : [Click Here](https://github.com/LISA-KOREA/YouTube-Video-Download-Bot)
-├🎓 **Developer** : [LISA 👑](https://t.me/LISA_FAN_LK)
+├👥 **Support Group** : [MY BOTS SUPPORT](https://t.me/MrBot02)
+├🥏 **Channel** : [MY BOT CHANNEL](https://t.me/PreetModzNetworkz/66)
+├⛲ **Source** : [Click Here](https://github.com/PREETCHAUHAN/YT-DOWNLOADER)
+├🎓 **Developer** : [☘️DJや๖ۣۜℜEET](https://t.me/djpreetxbot)
 ╰───────────────⍟
 """
