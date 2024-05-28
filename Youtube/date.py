8ad9c62c3327991fe2e45918277b870fbc197c0c
DJ Preet <33035404+PREETCHAUHAN@users.noreply.github.com>
2024-05-28 17:31:26 +0530

    Update date.py

--- Youtube/date.py
+++ Youtube/date.py
@@ -37,7 +37,7 @@ def date(client, message):
     }
 
     # Get the country from the message text if provided
-    country = "South Korea"  # Default to south korea
+    country = "India"  # Default to India
     if len(message.command) > 1:
         country = " ".join(message.command[1:])
     
